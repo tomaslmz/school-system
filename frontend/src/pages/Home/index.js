76528c276894476524ee4edf0334e77bcd2e76f9
tomas <tomas153lm@gmail.com>
2023-10-31 10:42:20 -0300

    listing students and feature to delete

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,21 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { FaUserAlt, FaUserGraduate } from 'react-icons/fa';
-import { Container, Button } from './styled';
+import {
+    FaPenSquare,
+    FaUserCircle,
+    FaExclamation,
+    FaWindowClose,
+    FaWind,
+} from 'react-icons/fa';
+
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { get } from 'lodash';
+import { Container, StudentContainer, Picture, Data } from './styled';
+import axios from '../../services/axios';
 
 export default function Home() {
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const response = await axios.get('/students/list');
+                setStudents(response.data);
+            } catch {
+                toast.error('Some error occurred trying to list students');
+            }
+        }
+
+        getData();
+    }, []);
+
+    async function handleDelete(e, index, id) {
+        try {
+            await axios.delete(`/students/delete/${id}`);
+            const newStudents = [...students];
+            newStudents.splice(index, 1);
+            setStudents(newStudents);
+        } catch {
+            toast.error('Some error occurred trying to delete a student!');
+        }
+    }
+
+    function handleAskDelete(e) {
+        e.preventDefault();
+        const exclamation = e.currentTarget.nextSibling;
+        exclamation.setAttribute('display', 'block');
+        e.currentTarget.remove();
+    }
+
     return (
         <Container>
-            <Button>
-                Manage users
-                <br />
-                <FaUserAlt size={50} />
-            </Button>
-            <Button>
-                Manage students
-                <br />
-                <FaUserGraduate size={50} />
-            </Button>
+            {students.map((student, index) => (
+                <StudentContainer id={String(student.id)}>
+                    <Picture>
+                        {get(student, 'Photos[0].url', false) ? (
+                            <img
+                                src={student.Photos[0].url}
+                                alt=""
+                                crossOrigin=""
+                            />
+                        ) : (
+                            <FaUserCircle size={36} />
+                        )}
+                    </Picture>
+                    <p>{student.name}</p>
+                    <p>{student.email}</p>
+
+                    <FaPenSquare color="#C3073F" cursor="pointer" size={16} />
+                    <Link
+                        to={`http:localhost:3001/students/delete/${student.id}`}
+                        onClick={handleAskDelete}
+                    >
+                        <FaWindowClose
+                            color="#C3073F"
+                            cursor="pointer"
+                            size={16}
+                        />
+                    </Link>
+
+                    <FaExclamation
+                        color="black"
+                        cursor="pointer"
+                        size={16}
+                        display="none"
+                        onClick={(e) => handleDelete(e, index, student.id)}
+                    />
+                </StudentContainer>
+            ))}
         </Container>
     );
 }
--- frontend/src/pages/Home/styled.js
+++ frontend/src/pages/Home/styled.js
@@ -3,10 +3,9 @@ import { primaryColor, primaryColorLight } from '../../config/colors';
 
 export const Container = styled.section`
     display: flex;
-    justify-content: center;
     align-items: center;
 
-    flex-direction: row;
+    flex-direction: column;
     gap: 20px;
 
     max-width: 600px;
@@ -15,6 +14,20 @@ export const Container = styled.section`
     margin: 30px auto;
     background-color: white;
     border-radius: 4px;
+`;
+
+export const StudentContainer = styled.section`
+    margin-top: 10px;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+
+    flex-direction: row;
+    gap: 10px;
+    flex-wrap: wrap;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #dedede;
 `;
 
 export const Button = styled.button`
@@ -29,3 +42,21 @@ export const Button = styled.button`
     background-color: ${primaryColor};
     box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
 `;
+
+export const Picture = styled.div`
+    img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+    }
+`;
+
+export const Data = styled.section`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    flex-direction: row;
+    gap: 10px;
+`;
+
+export const Options = styled.section``;
